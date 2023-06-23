b63f827a34341c2751cf55f7eef0d5c42d066c09
Szymon Wrobel <szymonwer567@gmail.com>
2023-06-23 19:22:35 +0200

    Add javadoc to School class

--- school/School.java
+++ school/School.java
@@ -5,23 +5,43 @@ import people.Teacher;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a school with teachers and students.
+ */
 public class School {
     private final ArrayList<Teacher> teachers;
     private final ArrayList<Student> students;
 
+    /**
+     * Constructs a School object with empty lists of teachers and students.
+     */
     public School() {
         teachers = new ArrayList<>();
         students = new ArrayList<>();
     }
 
+    /**
+     * Adds a teacher to the school.
+     *
+     * @param teacher The teacher to be added.
+     */
     public void addTeacher(Teacher teacher) {
         teachers.add(teacher);
     }
 
+    /**
+     * Adds a student to the school.
+     *
+     * @param student The student to be added.
+     */
     public void addStudent(Student student) {
         students.add(student);
     }
 
+    /**
+     * Grades every student by every teacher in the school.
+     * Prints the grading information for each student.
+     */
     public void gradeStudents() {
         for (Teacher teacher : teachers) {
             for (Student student : students) {
